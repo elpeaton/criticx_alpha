02b31236ae94816150737711abc445b9a26834f6
humberto delgado <dehumberto@gmail.com>
2020-05-27 14:28:20 -0500

    Task 6 - Use counter_cache for the field review_count

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: :review_count
   belongs_to :reviewable, polymorphic: true
 
-  after_create :add_review_count
-  after_destroy :subtract_review_count
+  # after_create :add_review_count
+  # after_destroy :subtract_review_count
 
-  def add_review_count
-    user = User.find_by(id: user_id)
-    user.update(review_count: user.review_count += 1)
-  end
+  # def add_review_count
+  #   user = User.find_by(id: user_id)
+  #   user.update(review_count: user.review_count += 1)
+  # end
 
-  def subtract_review_count
-    user = User.find_by(id: user_id)
-    user.update(review_count: user.review_count -= 1)
-  end
+  # def subtract_review_count
+  #   user = User.find_by(id: user_id)
+  #   user.update(review_count: user.review_count -= 1)
+  # end
 end
